0927d5f74c76f4879627780cfcd40af33121fa58
diarmuidmcg <47873953+diarmuidmcg@users.noreply.github.com>
2019-11-27 10:08:13 +0000

    cannot take invald  hit or Stand function
    Made sure that the function only takes valid input, otherwise the function starts recursively

--- BlackJack_src_hitOrStand.java
+++ BlackJack_src_hitOrStand.java
@@ -5,7 +5,9 @@ public class hitOrStand {
         System.out.println("Your score is " + score + ". Would you like to\n(H)it or\n(S)tand?");
         Scanner Input = new Scanner(System.in);
         String UserResponseHit = Input.next();
-        int newScore;
+        int newScore = 0;
+
+        // if player hits
         if (UserResponseHit.equalsIgnoreCase("Hit") || UserResponseHit.equalsIgnoreCase("H")) {
             newScore = hitCards(score);
             if (newScore > 21) {
@@ -17,12 +19,16 @@ public class hitOrStand {
                return newScore;
            } if (newScore < 21) {
                 int tryThisScore = newScore;
-                playBJ(tryThisScore);
+                newScore = playBJ(tryThisScore);
           }
+        }
 
-        } else if (UserResponseHit.equalsIgnoreCase("Stand") || UserResponseHit.equalsIgnoreCase("S")) {
+        // if player stands
+        else if (UserResponseHit.equalsIgnoreCase("Stand") || UserResponseHit.equalsIgnoreCase("S")) {
             newScore = score;
         }
+
+        // if wrong input
         else {
             System.out.println("try again");
             newScore = score;
@@ -30,6 +36,8 @@ public class hitOrStand {
         }
         return newScore;
     }
+
+    // simple function to get new card and add to existing score
     public static int hitCards(int score){
         String DealCard = generateCards.returnCards();
         System.out.println("You were dealt a: " + DealCard);
@@ -37,4 +45,9 @@ public class hitOrStand {
         int newScore = score + cardToNumber;
         return newScore;
     }
+//    public static String ensureUserResponseString() {
+//        Scanner Input = new Scanner(System.in);
+//        String UserResponseHit = Input.next();
+//    }
 }
+
