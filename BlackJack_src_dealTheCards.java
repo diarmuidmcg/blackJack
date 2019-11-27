291bfef80b7b04959259b5f0beb95595ccf20ccd
diarmuidmcg <47873953+diarmuidmcg@users.noreply.github.com>
2019-11-27 10:01:27 +0000

    Made the card dealings applicable to ace exception
    Cards now auto populate when the card given is an ace. Also shortened the dealOneCard function

--- BlackJack_src_dealTheCards.java
+++ BlackJack_src_dealTheCards.java
@@ -1,72 +1,79 @@
-import javax.print.DocFlavor;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class dealTheCards {
 
 // this creates an array with a length of how many players are playing
-    public static int[] createArrayForCards(int totalPlayers) {
-        int[] cardValues = new int[totalPlayers];
+    public static int[][] createArrayForCards(int totalPlayers) {
+        int[][] cardValues = new int[totalPlayers][2];
         // this makes the default value zero. redunant yes, but wanted to populate it first
         for (int i = 0; i < cardValues.length; i++) {
-            cardValues[i] = 0;
+            for (int j = 0; j < 2 ; j++) {
+                cardValues[i][j] = 0;
+            }
         }
         return cardValues;
     }
 // this prints out the players cards and then adds the value to the array
-    public static int[] generateFirstCard(int[] cardValues) {
+
+    public static void generateFirstCard(int[][] cardValues) {
         for (int i = 1; i < cardValues.length + 1; i++) {
             String newCard = generateCards.returnCards();
             System.out.println("Player #" + i + "'s first card is a " + newCard);
-            int cardToNumber = dealOneCard(newCard);
-            cardValues[i - 1] = cardToNumber;
+
+            // exception case for ace
+            if (newCard.equalsIgnoreCase("ace")){
+                cardValues[i - 1][0] = 11;
+                cardValues[i - 1][1] = 1;
+            }
+            else {
+                int cardToNumber = dealOneCard(newCard);
+                cardValues[i - 1][0] = cardToNumber;
+            }
         }
-        System.out.println(Arrays.toString(cardValues));
-        return cardValues;
+        System.out.println(Arrays.deepToString(cardValues));
     }
-    public static int[] generateSecondCard(int[] cardValues) {
+    public static void generateSecondCard(int[][] cardValues) {
         for (int i = 1; i < cardValues.length + 1; i++) {
             String newCard = generateCards.returnCards();
             System.out.println("Player #" + i + "'s second card is a " + newCard);
-            int cardToNumber = dealOneCard(newCard);
-            int previousValue = cardValues[i - 1];
-            cardValues[i - 1] = cardToNumber + previousValue;
-            int newCardValue = cardValues[i - 1];
-            if (newCardValue > 49){
-                if ((cardValues[i-1] - 38) == 21)
-                {
-                    System.out.println("Blackjack!");
-                }
-                else {
+            int previousValue = cardValues[i - 1][0];
 
-                    System.out.println("Their total total score is " + (cardValues[i - 1] - 48)
-                            + " or " + (cardValues[i - 1] - 38) + "\n");
-                }
-            } else {
-                System.out.println("Their total total score is " + cardValues[i - 1] + "\n");
+            // ace exception if 2nd card is ace
+            if (newCard.equalsIgnoreCase("ace")) {
+                cardValues[i - 1][0] = 11 + previousValue;
+                cardValues[i - 1][1] = 1 + previousValue;
+            }
+            // ace exception if 1st card is ace
+            else if (cardValues[i - 1][1] > 0) {
+                int cardToNumber = dealOneCard(newCard);
+                cardValues[i - 1][0] = cardToNumber + previousValue;
+                cardValues[i - 1][1] = cardToNumber + previousValue - 10;
+            }
+            else {
+                int cardToNumber = dealOneCard(newCard);
+                cardValues[i - 1][0] = cardToNumber + previousValue;
+            }
+            // regular output
+            if (cardValues[i - 1][1] == 0) {
+                System.out.println("Their total total score is " + cardValues[i - 1][0] + "\n");
+            }
+            // ace exception output
+            else {
+                System.out.println("Their total total score is " + cardValues[i - 1][0]
+                        + " or " + cardValues[i - 1][1] + "\n");
             }
         }
-        System.out.println(Arrays.toString(cardValues));
-        return cardValues;
+        System.out.println(Arrays.deepToString(cardValues));
     }
+
     // simple function to deal cards
     public static int dealOneCard(String newCard){
         int cardToNumber;
-        if (newCard.equals("Jack")) {
-            cardToNumber = 10;
-        } else if (newCard.equals("Queen")) {
-            cardToNumber = 10;
-        }
-        else if (newCard.equals("King")) {
-            cardToNumber = 10;
-        }
-        else if (newCard.equals("Ace")) {
-            // create something that makes it a 50 50. perhaps make it equal to 50
-            cardToNumber = 49;
-            // cardToNumber = 50;
-        } else {
-            cardToNumber = Integer.parseInt(newCard);
-        }
+        if (newCard.equals("Jack"))         cardToNumber = 10;
+        else if (newCard.equals("Queen"))   cardToNumber = 10;
+        else if (newCard.equals("King"))    cardToNumber = 10;
+        else if (newCard.equals("Ace"))     cardToNumber = 1;
+        else                                cardToNumber = Integer.parseInt(newCard);
         return cardToNumber;
     }
 }
