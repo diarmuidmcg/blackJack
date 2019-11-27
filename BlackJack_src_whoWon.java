850a4bd7219dbbaf784e2e827b155a7419f0e31c
diarmuidmcg <47873953+diarmuidmcg@users.noreply.github.com>
2019-11-27 10:14:08 +0000

    Updated Parameters, output
    Took in the 2d player card values and made dealer final score into an int. With aceInTheHole, i made dealtHands[i][0] always equal the player final score to make comparison easier. Also explain reason the player won/lost/pushed

--- BlackJack_src_whoWon.java
+++ BlackJack_src_whoWon.java
@@ -1,20 +1,34 @@
 public class whoWon {
-    public static void theWinnerIs(int[] dealtHands, int dealerHand) {
-            for (int i = 0; i < dealtHands.length; i++){
-                int playerNumber = i + 1;
-                if (dealtHands[i] > 21){
-                    System.out.println("Player #" + playerNumber + " lost.");
-                }
-                else if (dealerHand > 21 && dealtHands[i] <= 21) {
-                    System.out.println("Player #" + playerNumber + " wins!");
-                }
-                else if (dealtHands[i] > dealerHand && dealtHands[i] <= 21){
-                    System.out.println("Player #" + playerNumber + " beat the dealer.");
-                } else if (dealtHands[i] == dealerHand){
-                    System.out.println("Player #" + playerNumber + " pushed.");
-                } else {
-                    System.out.println("Player #" + playerNumber + " lost.");
-                }
+    public static void theWinnerIs(int[][] dealtHands, int dealerHand) {
+
+        for (int i = 0; i < dealtHands.length; i++){
+            String finalComparison = "";
+            int playerNumber = i + 1;
+            if (dealtHands[i][0] > 21){
+                System.out.print("Player #" + playerNumber + " lost ");
+                finalComparison = "the player burst with a score of " + dealtHands[i][0];
             }
+            else if (dealerHand > 21 && dealtHands[i][0] <= 21) {
+                System.out.print("Player #" + playerNumber + " wins ");
+                finalComparison = "the dealer burst!";
+            }
+            else if (dealtHands[i][0] > dealerHand && dealtHands[i][0] <= 21){
+                System.out.print("Player #" + playerNumber + " beat the dealer ");
+                finalComparison = "" + dealtHands[i][0] + " is greater than " + dealerHand;
+            }
+            else if (dealtHands[i][0] < dealerHand && dealerHand <= 21) {
+                System.out.print("Player #" + playerNumber + " lost ");
+                finalComparison = "" + dealtHands[i][0] + " is less than " + dealerHand;
+            }
+            else if (dealtHands[i][0] == dealerHand){
+                System.out.print("Player #" + playerNumber + " pushed ");
+                finalComparison = "" + dealtHands[i][0] + " equals " + dealerHand;
+            }
+            else {
+                System.out.print("Player #" + playerNumber + " lost ");
+                finalComparison = "the player burst with a score of " + dealtHands[i][0];
+            }
+            System.out.println("because "  + finalComparison);
+        }
     }
 }
