542262bed1558e2cb87d4dec53bb1aae6c19eebe
diarmuidmcg <47873953+diarmuidmcg@users.noreply.github.com>
2019-11-27 10:10:42 +0000

    Added comments, updated method calls
    Added comments to explain how the game progresses, updated method calls to add 2d arrays for player cards and 1d array for dealer cards

--- BlackJack_src_main.java
+++ BlackJack_src_main.java
@@ -1,44 +1,66 @@
-import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
+
+// welcome to game, gets number of players
         int totalPlayers = numberOfPlayers.introToBJ();
         System.out.println("It is now time to deal cards.");
         spaceToProceed.actionRequired();
-        int[] firstCards = dealTheCards.createArrayForCards(totalPlayers);
-        int[] dealtFirstHand = dealTheCards.generateFirstCard(firstCards);
+        int[][] playerCards = dealTheCards.createArrayForCards(totalPlayers);
+
+// lets get the first batch of cards
+        dealTheCards.generateFirstCard(playerCards);
         System.out.println();
+
+// dealer will get her first card
         System.out.println("The dealer will now get her first card");
         spaceToProceed.actionRequired();
-        int dealerHand = dealerCards.firstDealerCard();
+        int[] dealerHand = dealerCards.firstDealerCard();
+        int dealerFirstCard = 0;
+        if (dealerHand[1] > 0) dealerFirstCard = dealerHand[1];
+        else dealerFirstCard = dealerHand[0];
         System.out.println();
+
+// lets get the second batch of card
         System.out.println("Now it is time for your second card");
         spaceToProceed.actionRequired();
-        int dealerHand2 = dealerCards.secondDealerCard();
-        int[] dealtSecondHand = dealTheCards.generateSecondCard(dealtFirstHand);
+
+// dealer will get ger second card
+        String dealerSecondCard = dealerCards.secondDealerCard(dealerHand);
+
+// players get second cards
+        dealTheCards.generateSecondCard(playerCards);
         spaceToProceed.actionRequired();
-        for (int i = 0; i < dealtSecondHand.length; i++){
-            String dealerFirstCard = generateCards.getFaceValueWithInt(dealerHand);
-            System.out.println("Remember, the dealers first card is a " + dealerFirstCard);
+
+// players can hit or stand. don't go over!
+        for (int i = 0; i < totalPlayers; i++){
+            String stringDealerFirstCard = generateCards.getFaceValue(dealerFirstCard);
+            System.out.println("Remember, the dealers first card is a " + stringDealerFirstCard);
             int playerNumberTurn = i + 1;
             int newScore = 0;
             System.out.println("\nPlayer " + playerNumberTurn + ": ");
-            if (dealtSecondHand[i] > 50)
-            {
-                newScore = (aceInTheHole.ifPlayerHasAce(dealtSecondHand[i])-49);
+            if (playerCards[i][0] == 21) {
+                System.out.println("BlackJack! You win");
+                newScore = 21;
             }
-            else {
-                newScore = hitOrStand.playBJ(dealtSecondHand[i]);
+            else if (playerCards[i][1] == 0) {
+                newScore = hitOrStand.playBJ(playerCards[i][0]);
             }
-            dealtSecondHand[i] = newScore;
-            System.out.println(dealtSecondHand[i]);
+            else newScore = aceInTheHole.ifPlayerHasAce(playerCards, i);
+
+            playerCards[i][0] = newScore;
+            System.out.println(newScore);
         }
         spaceToProceed.actionRequired();
-        System.out.println("The Dealers second card is a " + dealerHand2);
+
+// reveal dealer second card.
+        System.out.println("The Dealers second card is a " + dealerSecondCard);
         spaceToProceed.actionRequired();
-        int dealerScore = dealerHand + dealerHand2;
-        int dealthDealerHand = dealerCards.dealerHitsCard(dealerScore);
-        whoWon.theWinnerIs(dealtSecondHand, dealthDealerHand);
+
+// determines if players win or lose
+        int dealerScore = dealerCards.determineDealerFinalHand(dealerHand);
+        whoWon.theWinnerIs(playerCards, dealerScore);
     }
 
 }
+
