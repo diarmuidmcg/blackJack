8f88a189ce7dc908fcc9de406438520668e480a1
diarmuidmcg <47873953+diarmuidmcg@users.noreply.github.com>
2019-11-27 10:03:07 +0000

    Updated Dealer to ace exception
    Changed the dealer file to accept and output ace exceptions. Also changed dealer hand to 1d array instead of int value.

--- BlackJack_src_dealerCards.java
+++ BlackJack_src_dealerCards.java
@@ -1,33 +1,66 @@
 public class dealerCards {
-    public static int firstDealerCard(){
+
+    // gets the first dealer card
+    public static int[] firstDealerCard(){
         String newCard = generateCards.returnCards();
         System.out.println("The Dealers first card is a " + newCard);
+        int[] dealerCards = new int[2];
         int cardToNumber = dealTheCards.dealOneCard(newCard);
-        return cardToNumber;
+
+        // ace exception
+        if (cardToNumber == 1) {
+            dealerCards[0] = 11;
+            dealerCards[1] = 1;
+        }
+        else dealerCards[0] = cardToNumber;
+        return dealerCards;
     }
-    public static int secondDealerCard(){
+
+    // gets second dealer card
+    public static String secondDealerCard(int[] dealerCards){
         String newCard = generateCards.returnCards();
-        //System.out.println("The Dealers second card is a " + newCard);
         int cardToNumber = dealTheCards.dealOneCard(newCard);
-        return cardToNumber;
+
+        // ace exception if second card is ace
+        if (cardToNumber == 1) {
+            dealerCards[0] += 11;
+            dealerCards[1] += 1;
+        }
+        // ace exception if first card is ace
+        else if (dealerCards[1] > 0) {
+            dealerCards[0] += cardToNumber;
+            dealerCards[1] += cardToNumber;
+        }
+        else dealerCards[0] += cardToNumber;
+        return newCard;
     }
-    public static int dealerHitsCard(int dealersDealtHand){
-        while (dealersDealtHand < 17
-                || dealersDealtHand < 57 && dealersDealtHand > 50
-                || dealersDealtHand < 67 && dealersDealtHand > 62) {
+
+    // this loop has dealer hit cards until its above 17. not ready for aces
+    public static int dealerHitsCard(int[] dealerCards){
+        System.out.println("The dealer's score is a " + dealerCards[0]);
+        while (dealerCards[0] < 17) {
             String newCard = generateCards.returnCards();
             System.out.println("The dealer drew a " + newCard);
             int cardToNumber = dealTheCards.dealOneCard(newCard);
-            dealersDealtHand += cardToNumber;
-            System.out.println("The dealer's score is a " + dealersDealtHand);
+            dealerCards[0] += cardToNumber;
+            System.out.println("The dealer's score is a " + dealerCards[0]);
         }
-        if (dealersDealtHand > 50) {
-            System.out.println("The dealer's final score is a " + (dealersDealtHand - 49));
-        }
-        else {
-            System.out.println("The dealer's final score is a " + dealersDealtHand);
-        }
-        return dealersDealtHand;
+        return dealerCards[0];
     }
 
+    // this determines if the dealer needs to hit more cards
+    public static int determineDealerFinalHand(int[] dealerCards) {
+        int dealerFinalScore = 0;
+
+        // if dealer cards don't need to hit
+        if (dealerCards[0] > 17)        dealerFinalScore = dealerCards[0];
+        // if dealer cards need to be hit
+        else if (dealerCards[1] == 0)   dealerFinalScore = dealerHitsCard(dealerCards);
+        // if aces are involved
+        else dealerFinalScore = aceInTheHole.ifDealerHasAce(dealerCards);
+
+        System.out.println("The dealer's final score is a " + dealerFinalScore);
+        return dealerFinalScore;
+    }
 }
+
